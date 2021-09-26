b93b6a5dd643a2b512dfad10134dca863045bb86
aaronnzhang <zhangyihao319@gmail.com>
2021-09-26 14:10:47 +0800

    [feat] add Semigroup play

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -1,7 +1,15 @@
 package playground
 
+import cats.kernel.Semigroup
+
 object Playground {
   def main(args: Array[String]): Unit = {
+
     println("Hello World")
+
+    println(Semigroup[Int].combine(1, 2))
+    println(Semigroup[List[Int]].combine(List(1, 2, 3), List(4, 5, 6)))
+    println(Semigroup[Option[Int]].combine(Option(1), Option(2)))
+    println(Semigroup[Option[Int]].combine(Option(1), None))
   }
 }
