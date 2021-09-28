d99a0c5a921ec175b91e5fbbd3e9c16946b4aca5
Yihao Zhang <aaron@Yihaos-MacBook-Pro.local>
2021-09-28 17:49:47 +0800

    add more play

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -1,5 +1,7 @@
 package playground
 
+import cats.Monoid
+import cats.implicits.toFoldableOps
 import cats.kernel.Semigroup
 
 object Playground {
@@ -11,5 +13,11 @@ object Playground {
     println(Semigroup[List[Int]].combine(List(1, 2, 3), List(4, 5, 6)))
     println(Semigroup[Option[Int]].combine(Option(1), Option(2)))
     println(Semigroup[Option[Int]].combine(Option(1), None))
+
+    println(Monoid[Map[String, Int]].combineAll(List(Map("a" -> 1, "b" -> 2), Map("a" -> 3))))
+    println(Monoid[Map[String, Int]].combineAll(List()))
+
+    val l = List(1, 2, 3, 4, 5)
+    println(l.foldMap(identity))
   }
 }
